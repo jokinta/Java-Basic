d38f20dcdf13864b6ea8bf00a8e20f92d27b1fdb
Jovan Ivanov <jokinta011@gmail.com>
2018-11-22 23:49:37 +0200

    Swing

---
+++ Swing/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Jokinta");
+        jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
+        jFrame.setSize(400,300);
+        jFrame.setBackground(Color.red);
+        jFrame.setVisible(true);
+    }
+}
